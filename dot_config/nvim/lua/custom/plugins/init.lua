4ecc8195aea686af693bcadc054c94a3ebc983e3
David Webb <david.webb@eborsystems.com>
2025-02-19 09:54:29 +1030

    Add neovim configuration

---
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,16 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+  },
+  {
+    'artoj/qmake-syntax-vim',
+    lazy = false,
+  },
+  {
+    'jamestthompson3/nvim-remote-containers',
+  },
+}
