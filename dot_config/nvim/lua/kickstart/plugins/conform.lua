1c26cf550301967e6aa172e67d3f443faa676f41
David Webb <david@dpwlabs.com>
2025-05-27 21:49:02 +0930

    Update rustaceanvim
    Set default to prettierd

--- dot_config/nvim/lua/kickstart/plugins/conform.lua
+++ dot_config/nvim/lua/kickstart/plugins/conform.lua
@@ -36,8 +36,8 @@ return {
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        json = { 'prettier' },
-        yaml = { 'prettier' },
+        json = { 'prettierd' },
+        yaml = { 'prettierd' },
       },
     },
   },
